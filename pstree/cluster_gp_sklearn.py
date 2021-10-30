efccc8f42f073eaee0165dff548421f4f4fa3993
zhenlingcn <zhenlingcn@foxmail.com>
2021-10-30 12:38:35 +0800

    1. support Tournament DCD

--- pstree/cluster_gp_sklearn.py
+++ pstree/cluster_gp_sklearn.py
@@ -667,6 +667,8 @@ class GPRegressor(NormalizationRegressor):
             p.ind_tuple = ind_tuple
             log_dict.insert(ind_tuple, p.fitness.values)
         pop_size = len(population)
+        # assigning the crowding distance to each individual
+        population = selNSGA2(population, pop_size)
 
         # Begin the generational process
         for gen in range(1, ngen + 1):
@@ -1255,27 +1257,13 @@ def selTournament(individuals, k, tournsize, fit_attr="fitness"):
 
 
 def selTournamentDCD(individuals, k):
-    """Tournament selection based on dominance (D) between two individuals, if
-    the two individuals do not interdominate the selection is made
-    based on crowding distance (CD). The *individuals* sequence length has to
-    be a multiple of 4 only if k is equal to the length of individuals.
-    Starting from the beginning of the selected individuals, two consecutive
-    individuals will be different (assuming all individuals in the input list
-    are unique). Each individual from the input list won't be selected more
-    than twice.
-    This selection requires the individuals to have a :attr:`crowding_dist`
-    attribute, which can be set by the :func:`assignCrowdingDist` function.
+    """
+    A simplified version of the tournament selection operator based on dominance and crowding distance
+
     :param individuals: A list of individuals to select from.
-    :param k: The number of individuals to select. Must be less than or equal
-              to len(individuals).
+    :param k: The number of individuals to select.
     :returns: A list of selected individuals.
     """
-
-    if k > len(individuals):
-        raise ValueError("selTournamentDCD: k must be less than or equal to individuals length")
-
-    if k == len(individuals) and k % 4 != 0:
-        raise ValueError("selTournamentDCD: k must be divisible by four if k == len(individuals)")
 
     def tourn(ind1, ind2):
         if ind1.fitness.dominates(ind2.fitness):
@@ -1283,23 +1271,18 @@ def selTournamentDCD(individuals, k):
         elif ind2.fitness.dominates(ind1.fitness):
             return ind2
 
-        # if ind1.fitness.crowding_dist < ind2.fitness.crowding_dist:
-        #     return ind2
-        # elif ind1.fitness.crowding_dist > ind2.fitness.crowding_dist:
-        #     return ind1
+        if ind1.fitness.crowding_dist < ind2.fitness.crowding_dist:
+            return ind2
+        elif ind1.fitness.crowding_dist > ind2.fitness.crowding_dist:
+            return ind1
 
         if random.random() <= 0.5:
             return ind1
         return ind2
 
-    individuals_1 = random.sample(individuals, len(individuals))
-    # individuals_2 = random.sample(individuals, len(individuals))
-
     chosen = []
     for i in range(0, k, 2):
-        chosen.append(tourn(individuals_1[i], individuals_1[i + 1]))
-        chosen.append(tourn(individuals_1[i + 2], individuals_1[i + 3]))
-        # chosen.append(tourn(individuals_2[i], individuals_2[i + 1]))
-        # chosen.append(tourn(individuals_2[i + 2], individuals_2[i + 3]))
-
+        individuals_sample = random.sample(individuals, 4)
+        chosen.append(tourn(individuals_sample[0], individuals_sample[1]))
+        chosen.append(tourn(individuals_sample[2], individuals_sample[3]))
     return chosen
